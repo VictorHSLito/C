dfb6c15e088f6c73675fe7493b051393a072184d
Victor Hugo <victorferrarihugo@hotmail.com>
2024-03-08 22:41:37 -0300

    Resolução de alguns exercícios

---
+++ Exercícios/Capítulo 2/Exercícios.c
@@ -0,0 +1,59 @@
+/*Elabore um programa que escreva as mensagens “Início do programa” e “Fim” na
+tela, uma em cada linha, usando apenas um comando printf().*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*int main () {
+    printf("Inicio do programa\n");
+    printf("Fim do programa\n");
+    system("pause");
+    return 0;
+}
+*/
+
+/*Escreva um programa que leia um número inteiro e depois o imprima.*/
+
+/*int main () {
+    float x;
+    printf("Digite um numero: ");
+    scanf("%f", &x);
+    printf("O numero digitado foi: %f", x);
+}
+*/
+
+/*Escreva um programa que leia um número inteiro e depois imprima a mensagem
+“Valor lido:”, seguido do valor inteiro. Use apenas um comando printf().*/
+
+/*int main () {
+    int x;
+    scanf("%d", &x);
+    printf("Valor lido: %d", x);
+} 
+*/
+
+/*Faça um programa que leia um número inteiro e depois o imprima usando o operador
+“%f”. Veja o que aconteceu.*/
+
+/*int main () {
+    int x;
+    printf("Digite um numero inteiro: ");
+    scanf("%d", &x);
+    printf("O numero digitado foi: %f", x);  // x consta como valor 0, pois o tipo da variável é inteiro e
+    return 0;                                // e foi passado uma chamada para um valor do tipo float
+}
+*/
+
+/*Faça um programa que leia um valor do tipo float e depois o imprima usando o
+operador “%d”. Veja o que aconteceu.*/
+
+/*int main () {
+    float x;
+    printf("Digite um numero: ");
+    scanf("%f", &x);
+    printf("O valor digitado foi: %d", x); // Acontece a mesma coisa do exercício acima
+    return 0;
+}
+*/
+
+

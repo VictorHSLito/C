3f3ca3f30527570b15e5153b16b52cc18060fa27
Victor Hugo <victorferrarihugo@hotmail.com>
2024-05-29 10:27:39 -0300

    Continuação capítulo 8

--- Exercícios/Capítulo 8/Exercícios.c
+++ Exercícios/Capítulo 8/Exercícios.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 /*1 - Implemente um programa que leia o nome, a idade e o endereço de uma pessoa e
 armazene esses dados em uma estrutura. Em seguida, imprima na tela os dados da
 estrutura lida.*/
 
-typedef struct {
+/*typedef struct {
     char nome[20];
     int idade;
     char endereco[50];
@@ -22,5 +23,26 @@ int main () {
     fgets(c.endereco, sizeof(c.endereco), stdin);
 
     printf("Voce se chama %s, tem %d anos de idade e mora no seguinte endereco: %s", c.nome, c.idade, c.endereco);
+    return 0;
+}
+*/
+
+/*2 - Crie uma estrutura para representar as coordenadas de um ponto no plano (posições
+X e Y). Em seguida, declare e leia do teclado um ponto e exiba a distância
+dele até a origem das coordenadas, isto é, a posição (0,0).*/
+
+struct origem {
+    int x, y;
+};
+
+int main () {
+    struct origem o;
+    int x = 0, y = 0;
+    float distancia;
+
+    printf("Digite dois valores inteiros para uma coordenada no plano cartesiano: ");
+    scanf("%d%d", &o.x, &o.y);
+    distancia = sqrt((pow(o.x, 2) - x) + (pow(o.y, 2) - y));
+    printf("A distancia do ponto digitado ate a origem (0, 0) eh: %f", distancia);
     return 0;
 }

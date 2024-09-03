15c3dea7a9d5723a60904135a9f1a4a85cd4cc0c
Victor Hugo <victorferrarihugo@hotmail.com>
2024-09-02 22:58:37 -0300

    Continuação dos Exercícios

---
+++ Exercícios/Capítulo 11/ex03.c
@@ -0,0 +1,6 @@
+/*Crie uma estrutura chamada Cadastro. Essa estrutura deve conter o nome, a idade
+e o endereço de uma pessoa. Agora, escreva uma função que receba um inteiro
+positivo N e retorne o ponteiro para um vetor de tamanho N, alocado dinamicamente,
+dessa estrutura. Solicite também que o usuário digite os dados desse vetor
+dentro da função.*/
+
